12821ef3d47ecc33c9845f4bf9f02a472032fbf7
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-08-08 15:42:19 +0900

    08.08 대시보드 가안 완성

--- flamengo/lib/screens/dashboard/dashboard_tab.dart
+++ flamengo/lib/screens/dashboard/dashboard_tab.dart
@@ -1,4 +1,7 @@
+import 'package:flamengo/constants/gaps.dart';
+import 'package:flamengo/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DashBoardScreen extends StatefulWidget {
   const DashBoardScreen({super.key});
@@ -10,6 +13,116 @@ class DashBoardScreen extends StatefulWidget {
 class _DashBoardScreenState extends State<DashBoardScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: Sizes.size28,
+            vertical: Sizes.size48,
+          ),
+          child: Column(
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 50,
+                        foregroundImage: NetworkImage(
+                            "https://lh3.googleusercontent.com/a/AGNmyxamUvm-3XN71fNXENMkFOcuBM1YTGv4RKiqqEd09g=s432-c-no"),
+                        child: Text("Carl"),
+                      ),
+                      Gaps.v8,
+                      Text(
+                        "@User",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("data"),
+                      Gaps.v8,
+                      Divider(),
+                      Gaps.v8,
+                      Text("data"),
+                      Gaps.v8,
+                      Divider(),
+                      Gaps.v8,
+                      Text("data"),
+                      Gaps.v8,
+                      Divider(),
+                      Gaps.v8,
+                    ],
+                  ),
+                ],
+              ),
+              Gaps.v36,
+              Text(
+                "My Favorite Site",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: Sizes.size24,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              Gaps.v36,
+              Card(
+                elevation: 8,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.amber.shade200,
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: Sizes.size12,
+                      vertical: Sizes.size12,
+                    ),
+                    child: ListView.separated(
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text("Item $index"),
+                          leading: FaIcon(
+                            FontAwesomeIcons.solidStar,
+                            color: Theme.of(context).primaryColor,
+                          ),
+                        );
+                      },
+                      separatorBuilder: (context, index) => const Divider(),
+                      itemCount: 10,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Divider extends StatelessWidget {
+  const Divider({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: Sizes.size1,
+      color: Colors.grey.shade400,
+    );
   }
 }
--- flamengo/lib/screens/mainnavigation/main_navigation_screen.dart
+++ flamengo/lib/screens/mainnavigation/main_navigation_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flamengo/screens/dashboard/dashboard_tab.dart';
+import 'package:flamengo/screens/information/travel_information_screen.dart';
 import 'package:flamengo/screens/mainnavigation/widget/main_appbar.dart';
+import 'package:flamengo/screens/recommend/local_recommend.dart';
+import 'package:flamengo/screens/schedule/travel_schedule.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,20 +17,6 @@ class MainNavigationScreen extends StatefulWidget {
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
   int _selectedIndex = 0;
-  final List _tabs = [
-    const Center(
-      child: Text('Dashboard'),
-    ),
-    const Center(
-      child: Text('Information'),
-    ),
-    const Center(
-      child: Text('Recommend'),
-    ),
-    const Center(
-      child: Text('Schedule'),
-    )
-  ];
 
   void _onTap(int index) {
     setState(() {
@@ -38,7 +28,26 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const MainAppBar(),
-      body: _tabs[_selectedIndex],
+      body: Stack(
+        children: [
+          Offstage(
+            offstage: _selectedIndex != 0,
+            child: const DashBoardScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 1,
+            child: const TravelInformationScreen(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 2,
+            child: const LocalRecommend(),
+          ),
+          Offstage(
+            offstage: _selectedIndex != 3,
+            child: const TravelScheduleScreen(),
+          ),
+        ],
+      ),
       bottomNavigationBar: NavigationBar(
         labelBehavior: NavigationDestinationLabelBehavior.onlyShowSelected,
         backgroundColor: Theme.of(context).primaryColor,
