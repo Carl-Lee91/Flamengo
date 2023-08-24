0bace9d523285c250f171b1de5a73720c47f7bb1
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-08-24 19:56:09 +0900

    추가: constant function 추가
    snackbar를 만들기 위해 재사용 가능한 function을 추가로 만들었음

--- flamengo/lib/constants/function.dart
+++ flamengo/lib/constants/function.dart
@@ -1,7 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Functions {
   static void onScaffoldTap(BuildContext context) {
     FocusScope.of(context).unfocus();
+  }
+
+  static void showErrorMessage(BuildContext context, Object? error) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        showCloseIcon: true,
+        content: Text((error as FirebaseException).message ?? "Error"),
+      ),
+    );
   }
 }
