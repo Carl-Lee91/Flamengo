90b3f8d5014111346f03b90e3f2153f65df4845c
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-10-03 20:00:38 +0900

    리팩터링: 세팅스크린
    완료

--- flamengo/lib/screens/setting/settings.dart
+++ flamengo/lib/screens/setting/settings.dart
@@ -1,3 +1,4 @@
+import 'package:flamengo/screens/authentication/repos/authentication_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -9,6 +10,32 @@ class SettingScreen extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Settings"),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text("Log Out"),
+            textColor: Theme.of(context).primaryColor,
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text("Are You Sure?"),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          ref.read(authRepo).signOut();
+                        },
+                        child: const Text("Yes"))
+                  ],
+                ),
+              );
+            },
+          ),
+          const AboutListTile(
+            applicationVersion: "1.0",
+          )
+        ],
       ),
     );
   }
