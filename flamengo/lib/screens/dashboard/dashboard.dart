812c654e90f5548f35adbfdb0c93053b803c0d00
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-10-03 20:00:04 +0900

    리팩터링: 대시보드
    대시보드 개정

--- flamengo/lib/screens/dashboard/dashboard.dart
+++ flamengo/lib/screens/dashboard/dashboard.dart
@@ -1,11 +1,9 @@
 import 'package:flamengo/constants/gaps.dart';
 import 'package:flamengo/constants/sizes.dart';
 import 'package:flamengo/screens/dashboard/widget/avatar.dart';
-import 'package:flamengo/screens/dashboard/widget/divider.dart';
 import 'package:flamengo/screens/users/view_models/users_view_models.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DashBoardScreen extends ConsumerStatefulWidget {
   const DashBoardScreen({
@@ -29,99 +27,67 @@ class _DashBoardScreenState extends ConsumerState<DashBoardScreen> {
             child: CircularProgressIndicator.adaptive(),
           ),
           data: (data) => Scaffold(
-            body: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Sizes.size28,
-                  vertical: Sizes.size48,
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Avatar(
-                              name: data.name,
-                              hasAvatar: data.hasAvatar,
-                              uid: data.uid,
-                            ),
-                            Gaps.v8,
-                            Text(
-                              "@${data.name}",
-                              style: const TextStyle(
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text("data"),
-                            Gaps.v8,
-                            DashboardDivider(),
-                            Gaps.v8,
-                            Text("data"),
-                            Gaps.v8,
-                            DashboardDivider(),
-                            Gaps.v8,
-                            Text("data"),
-                            Gaps.v8,
-                            DashboardDivider(),
-                            Gaps.v8,
-                          ],
-                        ),
-                      ],
-                    ),
-                    Gaps.v36,
-                    Text(
-                      "My Travel Schedule",
-                      style: TextStyle(
-                        color: Theme.of(context).primaryColor,
-                        fontSize: Sizes.size24,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                    Gaps.v36,
-                    Card(
-                      elevation: 8,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 400,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: Colors.amber.shade200,
-                        ),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: Sizes.size12,
-                            vertical: Sizes.size12,
+            body: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size28,
+                vertical: Sizes.size48,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Column(
+                        children: [
+                          Avatar(
+                            name: data.name,
+                            hasAvatar: data.hasAvatar,
+                            uid: data.uid,
                           ),
-                          child: ListView.separated(
-                            itemBuilder: (context, index) {
-                              return ListTile(
-                                title: Text("Item $index"),
-                                leading: FaIcon(
-                                  FontAwesomeIcons.solidCircle,
-                                  size: Sizes.size10,
-                                  color: Theme.of(context).primaryColor,
-                                ),
-                              );
-                            },
-                            separatorBuilder: (context, index) =>
-                                const DashboardDivider(),
-                            itemCount: 10,
+                          Gaps.v8,
+                          Text(
+                            "@${data.name}",
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w600,
+                            ),
                           ),
-                        ),
+                        ],
                       ),
+                    ],
+                  ),
+                  Gaps.v36,
+                  Text(
+                    "Welcome ${data.name}",
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontSize: Sizes.size24,
+                      fontWeight: FontWeight.w600,
                     ),
-                  ],
-                ),
+                  ),
+                  Gaps.v36,
+                  const Text(
+                    "Enjoy Your Trip!",
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const Text(
+                    "Press information to search store",
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const Text(
+                    "Press recommend to view your favorite store",
+                    style: TextStyle(
+                      fontSize: Sizes.size16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
