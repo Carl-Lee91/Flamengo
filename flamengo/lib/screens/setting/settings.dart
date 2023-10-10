5c238dae538b671416dddba6f42626fc3ffdd6a7
Carl-Lee91 <112924805+Carl-Lee91@users.noreply.github.com>
2023-10-10 09:14:12 +0900

    리팩터링: 세팅스크린
    마지막 작업

--- flamengo/lib/screens/setting/settings.dart
+++ flamengo/lib/screens/setting/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flamengo/screens/authentication/repos/authentication_repo.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class SettingScreen extends ConsumerWidget {
   const SettingScreen({super.key});
@@ -23,8 +24,12 @@ class SettingScreen extends ConsumerWidget {
                   title: const Text("Are You Sure?"),
                   actions: [
                     TextButton(
+                        onPressed: () => Navigator.of(context).pop(),
+                        child: const Text("No")),
+                    TextButton(
                         onPressed: () {
                           ref.read(authRepo).signOut();
+                          context.go("/");
                         },
                         child: const Text("Yes"))
                   ],
