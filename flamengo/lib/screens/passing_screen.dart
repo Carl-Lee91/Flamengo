6ef71703b66c7865ea5ef8fa8674f70b3c2b4edd
Carl Lee <cj91@hanmail.net>
2023-12-07 17:06:26 +0900

    refactor: to use location services

--- flamengo/lib/screens/passing_screen.dart
+++ flamengo/lib/screens/passing_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flamengo/constants/gaps.dart';
 import 'package:flamengo/constants/sizes.dart';
 import 'package:flamengo/screens/authentication/view_models/signup_view_models.dart';
@@ -5,6 +7,7 @@ import 'package:flamengo/screens/authentication/widget/form_btn.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class PassingScreen extends ConsumerStatefulWidget {
   static String routeName = "tutorial";
@@ -18,7 +21,40 @@ class PassingScreen extends ConsumerStatefulWidget {
 
 class _PassingScreenState extends ConsumerState<PassingScreen> {
   Future<void> _onEnterAppTab() async {
-    context.go("/dashboard");
+    PermissionStatus status = await Permission.location.status;
+    status = await Permission.location.request();
+    if (!mounted) return;
+    if (status.isGranted) {
+      context.go("/dashboard");
+    } else {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text(
+              "위치 정보 권한 확인",
+            ),
+            content: const Text(
+              "위치 정보 권한을 허가하여 주십시오.",
+            ),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  status = await Permission.location.request();
+                  if (!mounted) return;
+                  if (status.isGranted) {
+                    context.go("/dashboard");
+                  }
+                },
+                child: const Text(
+                  "허가",
+                ),
+              ),
+            ],
+          );
+        },
+      );
+    }
   }
 
   @override
